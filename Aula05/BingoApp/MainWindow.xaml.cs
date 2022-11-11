352176f1c6b8a88da617badc1208c00628d0a233
Gilbert Azevedo da Silva <1164676@ifrn.local>
2022-11-11 15:25:32 -0300

    Bingo

--- Aula05/BingoApp/MainWindow.xaml.cs
+++ Aula05/BingoApp/MainWindow.xaml.cs
@@ -30,15 +30,35 @@ namespace BingoApp
             txtNumBolas.Text = slider.Value.ToString();
         }
 
+        private Bingo b;
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Iniciar");
-            // Comentário
+            b = new Bingo();
+            b.Iniciar(int.Parse(txtNumBolas.Text));
+            btnIniciar.IsEnabled = false;
+            btnSortear.IsEnabled = true;
+            txtNum.Clear();
+            txtSorteados.Clear();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Sortear");
+            int x = b.Sortear();
+            if (x == -1)
+            {
+                txtNum.Text = "Fim";
+                btnIniciar.IsEnabled = true;
+                btnSortear.IsEnabled = false;
+            }
+            else
+            {
+                txtNum.Text = x.ToString();
+                string s = "";
+                foreach (int i in b.Sorteados())
+                    s = s + i + " - ";
+                txtSorteados.Text = s;
+            }
         }
     }
 }
