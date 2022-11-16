b6b803a06a8754aaf995ba23bf915570b978b00c
Gilbert-Silva <gilbertif1995@outlook.com>
2022-11-16 11:12:08 -0300

    Cap 16_11

---
+++ Cap_1116/ExemploApp/Aluno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploApp
+{
+    class Aluno
+    {
+        private string nome = "Sem nome";
+        public Aluno(string nome)
+        {
+            if (nome != "") this.nome = nome;
+            //if (nome != string.Empty) this.nome = nome;
+        }
+        public string Iniciais()
+        {
+            string[] vs = nome.Split();
+            string r = "";
+            foreach (string palavra in vs)
+                r = r + palavra[0];
+            return r;
+        }
+        public int[] Senha()
+        {
+            int[] vetor = new int[4];
+            vetor[0] = 1000;
+            vetor[1] = 1234;
+            vetor[2] = 4321;
+            vetor[3] = 9001;
+            return vetor;
+        }
+    }
+}
---
+++ Cap_1116/ExemploApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExemploApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Aluno obj;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            obj = new Aluno(txt1.Text);
+            txt2.Text = obj.Iniciais();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int[] vetor = obj.Senha();
+            Random r = new Random();
+            int x = r.Next(0, vetor.Length);
+            txt2.Text = vetor[x].ToString();
+        }
+    }
+}
