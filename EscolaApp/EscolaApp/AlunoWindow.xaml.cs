6be6b6bd4885657c50416b645543ed8da8dd053a
Gilbert Azevedo da Silva <gilbertif1995@outlook.com>
2023-01-09 15:46:00 -0300

    Crud de Aluno

--- EscolaApp/EscolaApp/AlunoWindow.xaml.cs
+++ EscolaApp/EscolaApp/AlunoWindow.xaml.cs
@@ -23,5 +23,62 @@ namespace EscolaApp
         {
             InitializeComponent();
         }
+
+        private void InserirClick(object sender, RoutedEventArgs e)
+        {
+            // Novo objeto com os dados da turma que será inserida
+            Aluno t = new Aluno();
+            t.Id = int.Parse(txtId.Text);
+            t.Nome = txtNome.Text;
+            t.Matricula = txtMatricula.Text;
+            t.Email = txtEmail.Text;
+            // Inserir a turma na lista de turmas
+            NAluno.Inserir(t);
+            // Lista a turma inserida
+            ListarClick(sender, e);
+        }
+
+        private void ListarClick(object sender, RoutedEventArgs e)
+        {
+            listAlunos.ItemsSource = null;
+            listAlunos.ItemsSource = NAluno.Listar();
+        }
+
+        private void AtualizarClick(object sender, RoutedEventArgs e)
+        {
+            // Novo objeto com os dados da turma que será inserida
+            Aluno t = new Aluno();
+            t.Id = int.Parse(txtId.Text);
+            t.Nome = txtNome.Text;
+            t.Matricula = txtMatricula.Text;
+            t.Email = txtEmail.Text;
+            // Inserir a turma na lista de turmas
+            NAluno.Atualizar(t);
+            // Lista as turmas cadastradas
+            ListarClick(sender, e);
+        }
+
+        private void ExcluirClick(object sender, RoutedEventArgs e)
+        {
+            // Novo objeto com os dados da turma que será inserida
+            Aluno t = new Aluno();
+            t.Id = int.Parse(txtId.Text);
+            // Inserir a turma na lista de turmas
+            NAluno.Excluir(t);
+            // Lista as turmas cadastradas
+            ListarClick(sender, e);
+        }
+
+        private void listAlunos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listAlunos.SelectedItem != null)
+            {
+                Aluno obj = (Aluno)listAlunos.SelectedItem;
+                txtId.Text = obj.Id.ToString();
+                txtNome.Text = obj.Nome;
+                txtMatricula.Text = obj.Matricula;
+                txtEmail.Text = obj.Email;
+            }
+        }
     }
 }
---
+++ EscolaApp/EscolaApp/NAluno.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EscolaApp
+{
+    class NAluno
+    {
+        private static List<Aluno> alunos = new List<Aluno>();
+        public static void Inserir(Aluno t)
+        {
+            Abrir();
+            alunos.Add(t);
+            Salvar();
+        }
+        public static List<Aluno> Listar()
+        {
+            Abrir();
+            return alunos;
+        }
+        public static void Atualizar(Aluno t)
+        {
+            Abrir();
+            // Percorrer a lista de turma procurando o id informado (t.Id)
+            foreach (Aluno obj in alunos)
+                if (obj.Id == t.Id)
+                {
+                    obj.Nome = t.Nome;
+                    obj.Matricula = t.Matricula;
+                    obj.Email = t.Email;
+                }
+            Salvar();
+        }
+        public static void Excluir(Aluno t)
+        {
+            Abrir();
+            // Percorrer a lista de turma procurando o id informado (t.Id)
+            Aluno x = null;
+            foreach (Aluno obj in alunos)
+                if (obj.Id == t.Id) x = obj;
+            if (x != null) alunos.Remove(x);
+            Salvar();
+        }
+        public static void Abrir()
+        {
+            StreamReader f = null;
+            try
+            {
+                // Objeto que serializa (transforma) uma lista de alunos em um texto em XML
+                XmlSerializer xml = new XmlSerializer(typeof(List<Aluno>));
+                // Objeto que abre um texto em um arquivo
+                f = new StreamReader("./alunos.xml");
+                // Chama a operação de desserialização informando a origem do texto XML
+                alunos = (List<Aluno>)xml.Deserialize(f);
+            }
+            catch
+            {
+                alunos = new List<Aluno>();
+            }
+            // Fecha o arquivo
+            if (f != null) f.Close();
+        }
+        public static void Salvar()
+        {
+            // Objeto que serializa (transforma) uma lista de alunos em um texto em XML
+            XmlSerializer xml = new XmlSerializer(typeof(List<Aluno>));
+            // Objeto que grava um texto em um arquivo
+            StreamWriter f = new StreamWriter("./alunos.xml", false);
+            // Chama a operação de serialização informando o destino do texto XML
+            xml.Serialize(f, alunos);
+            // Fecha o arquivo
+            f.Close();
+        }
+    }
+}
